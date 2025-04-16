ef6b99e952426508c86f9d19157447b86c2b6aa3
Lucas Safeno <lugnorio@gmail.com>
2025-04-16 09:57:30 -0300

    PHP para iniciantes#07 - Operadores de atribuição

---
+++ public/operadores_atribuicao.php
@@ -0,0 +1,17 @@
+<?php
+
+// atribuição =
+$name = 'Lucas';
+
+// concatenação
+//$name = $name . 'Tenório';
+$name .= ' Tenório';
+
+
+$number = 10;
+
+$number += 20;
+
+echo $number;
+
+//echo $name;
