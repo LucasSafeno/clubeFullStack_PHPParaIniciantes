407d8f6f1a8e4b6ef331deae1d1be140241ca0e8
Lucas Safeno <lugnorio@gmail.com>
2025-04-16 09:06:03 -0300

    PHP para iniciantes#06 - Operadores aritméticos

---
+++ public/operadores_aritimetricos.php
@@ -0,0 +1,23 @@
+<?php
+
+$numer1 = 10;
+$number = 20;
+
+
+//? soma
+echo $numer1 + $numer2;
+//? subtração
+echo $numer2 - $numer1;
+//? multiplicação
+echo $numer1 * $numer2;
+//? divisão
+echo $numer1 / $numer2;
+//? resto
+echo $number1 % $numer2;
+
+
+//* precedencia de operadores = https://www.php.net/manual/pt_BR/language.operators.precedence.php
+$operacao = $number1 * $numer2 + 5;
+
+echo $operacao;
+?>
