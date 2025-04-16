6e1240ad7c0eeafea2a52aa470588711294802dc
Lucas Safeno <lugnorio@gmail.com>
2025-04-16 08:49:12 -0300

    PHP para iniciantes#03 - Variáveis, seus tipos de dados e referência

---
+++ public/variaveis.php
@@ -0,0 +1,40 @@
+<?php
+
+
+//? variavei pode começar com _ | Não pode começar com número
+
+$myNameIs = 'Lucas'; // CamelCase
+$name_is = 'Tenório'; // snake_case
+$number = 30;
+
+echo $myname;
+
+
+// //? strings
+// //* Tudo que você colocar entre aspas simples ou duplas
+// echo gettype("tipo String");
+// echo "<br>";
+// //? numbers - integer, float(double) 
+// echo gettype(30);
+// echo "<br>";
+// echo gettype(12.50);
+// echo "<br>";
+// //? boolean
+// echo gettype(true);
+// echo "<br>";
+// //? array
+// echo gettype(['indice' => '10']);
+// echo "<br>";
+// //? object
+// class Person
+// {
+//     public function teste()
+//     {
+//         echo "Class Person";
+//     }
+// }
+
+// echo gettype(new Person());
+
+// //? null
+// echo gettype(null);
