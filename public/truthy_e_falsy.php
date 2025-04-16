409823a9110c9eb9429e65574032ba21a9bedd7e
Lucas Safeno <lugnorio@gmail.com>
2025-04-16 10:00:41 -0300

    PHP para iniciantes#11 - Truthy e Falsy

---
+++ public/truthy_e_falsy.php
@@ -0,0 +1,11 @@
+<?php
+// falsy ->  Valores que se comportam como balores booleanos falsos, mas não são falsos
+// Truthy
+
+
+
+var_dump(!!0.0);
+var_dump(!!"0");
+var_dump(!!"");
+var_dump(!![]);
+?>
