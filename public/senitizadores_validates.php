a627b0c60a48b9402b85b028fab08ec6dfb1cc10
Lucas Safeno <lugnorio@gmail.com>
2025-04-22 11:58:27 -0300

    PHP para iniciantes#36 - Sanitizadores e Validates

---
+++ public/senitizadores_validates.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pegando arquivos enviando pelo formulário</title>
+</head>
+
+<body>
+    <form action="validate.php" method="post">
+        <input type="text" name="name" id="name" placeholder="name"> <br>
+        <input type="email" name="email" id="email" placeholder='email'>
+        <button type="submit">Cadastrar</button>
+    </form>
+
+</body>
+
+</html>
